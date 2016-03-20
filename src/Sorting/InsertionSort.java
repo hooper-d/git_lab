5127bae7665bfecd68eb4d80be1d2890e0474d36
Zach Dixon <zachary_dixon@brown.edu>
2016-03-20 15:27:02 -0400

    Implemented Insertion Sort.

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -8,8 +8,20 @@ public class InsertionSort {
 	 * @return					the input array, sorted least to greatest
 	 */
 	public static int[] insertionSort(int[] unsortedArray) {
-		//TODO: S2 implements insertion sort
-		
-		return null;
+		nt n = unsortedArray.length;
+	  if (n == 0 || n == 1) {
+		  return unsortedArray;
+	  }
+	  int i;
+	  int j;
+	  int tmp;
+	  for (i = 1; i < n; i++) {
+		  tmp = unsortedArray[i];
+		  for (j = i - 1; (j >= 0) && unsortedArray[j] > tmp; j--) {
+			  unsortedArray[j + 1] = unsortedArray[j];
+		  }
+		  unsortedArray[j + 1] = tmp;
+	  }
+    return unsortedArray;
 	}
 }
--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -2,31 +2,29 @@ package Sorting;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		//Generates a random array
-		int[] unsortedArray = new int[(int) (Math.random()*20.0)];
-		for (int i = 0; i < unsortedArray.length; i++) {
-			unsortedArray[i] = (int) (Math.random()*20.0);
-		}
-		
-		System.out.println("Unsorted Array:");
-		
-		for (int i: unsortedArray) {
-				System.out.print(i + " ");
-		}
-		System.out.println();
-		
-		System.out.println("Sorted Array:");
-		//TODO: sort the array and print out its contents
-		BubbleSort.bubbleSort(unsortedArray);
-		for (int i: unsortedArray) {
-			System.out.print(i + " ");
-		}
-		System.out.println();
-	}
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
 
+    //Generates a random array
+    int[] unsortedArray = new int[(int) (Math.random()*20.0)];
+    for (int i = 0; i < unsortedArray.length; i++) {
+      unsortedArray[i] = (int) (Math.random()*20.0);
+    }
+
+    System.out.println("Unsorted Array:");
+
+    for (int i: unsortedArray) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    InsertionSort.insertionSort(unsortedArray);
+    System.out.println("Sorted Array:");
+    for (int i: unsortedArray) {
+      System.out.print(i + " ");
+    }
+
+
+  }
 }
