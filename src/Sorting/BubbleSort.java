58b8eda039e9ecb84d66d9388789ea82052cbac0
Dave Hooper <david_hooper@brown.edu>
2016-03-20 15:22:11 -0400

    Implemented Bubble Sort.

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -8,7 +8,29 @@ public class BubbleSort {
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
 		//TODO: S1 implements bubble sort
+		if(unsortedArray == null) {
+			return unsortedArray;
+		}
 		
-		return null;
+		int size = unsortedArray.length;
+		if(size <= 1) {
+			return unsortedArray;
+		}
+		
+		int tmp;
+		boolean sorted = false;
+		while(sorted == false) {
+			sorted = true;
+			for(int i = 1; i < size; i++) {
+				if(unsortedArray[i] < unsortedArray[i-1]) {
+					sorted = false;
+					tmp = unsortedArray[i];
+					unsortedArray[i] = unsortedArray[i-1];
+					unsortedArray[i-1] = tmp;
+				}
+			}
+			size--;
+		}
+	    return unsortedArray;
 	}
 }
